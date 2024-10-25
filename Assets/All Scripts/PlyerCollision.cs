df7662ed5a88edae47d31fc078787afdad8260f4
Hadi493 <hadialam493@gmail.com>
2024-10-25 22:40:56 +0600

    added a new function destroy cube

---
+++ Assets/All Scripts/PlyerCollision.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlyerCollasion : MonoBehaviour {
+    
+    // public PlyerScript plyerScript;
+
+    // private void OnCollisionEnter(Collision other) {
+    //     // Debug.Log(other.gameObject.name);
+    //     if (other.gameObject.tag == "Collectables") {
+    //         Destroy(other.gameObject);
+    //     }
+    // }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Collectables") {
+            Destroy(other.gameObject);
+        }
+    }
+
+    // private void OnCollisionEnter(Collision other) {
+    //     if (other.gameObject.tag == "Obstracles") {
+    //         plyerScript.enabled = false;
+    //     }
+    // }
+}
